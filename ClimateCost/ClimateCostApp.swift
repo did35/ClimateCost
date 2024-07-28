78d928c9e82165097a52c61f2db77c398e41a8e9
Didier <did35@yahoo.com>
2024-07-28 17:22:26 +0200

    Initial Commit

---
+++ ClimateCost/ClimateCostApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClimateCostApp.swift
+//  ClimateCost
+//
+//  Created by Didier Delhaisse on 28/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct ClimateCostApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
